c0e14424827b242b0e18461a3e0686d1fe48250b
naywilkins512 <nathanwilkins01@gmail.com>
2020-08-11 14:14:19 -0500

    adds script for starting quiz and cycling questions

---
+++ Assets/script.js
@@ -0,0 +1,72 @@
+
+
+let timer = 30
+let questions = [
+    {
+        question: "what is javascript?", 
+        choices: ["javascript", "html", "css", "java"],
+        answer: "javascript"
+    },
+    {
+        question: "what is html?", 
+        choices: ["javascript", "html", "css", "java"],
+        answer: "html"
+    },
+    {
+        question: "what is css?", 
+        choices: ["javascript", "html", "css", "java"],
+        answer: "css"
+    },
+    {
+        question: "what is java?", 
+        choices: ["javascript", "html", "css", "java"],
+        answer: "java"
+    },
+]
+
+let currentQuestion = 0 
+let interval = 0
+
+let submitButton = document.getElementById("submitter") //move to top when done
+let timerText = document.getElementById("timerText") // move to top
+let questionAsked = document.getElementById("questionAsked") // move to top
+let choiceButtons = document.getElementsByClassName("choice") // move to top later
+
+submitButton.addEventListener("click", checkQuestion)
+
+function startQuiz() {
+    interval = setInterval(function() {
+        
+        // So renderTime() is called here once every second.
+        if (timer > 0) {
+            timer--;
+        }
+        else {
+            clearInterval(interval)
+        }
+        renderTime();
+      }, 1000);
+      askQuestion()
+}
+startQuiz()
+
+function checkQuestion() {
+    // check answer, if there are more questions: currentquestion++ run askquestion() 
+    // else end quiz aka don't increment questions show score stop timer--
+    // if right add to score
+    // if wrong run question timer-5
+}
+
+
+
+function renderTime() {
+    timerText.textContent = timer
+}
+
+function askQuestion() {
+    questionAsked.textContent = questions[currentQuestion].question
+    for (let i = 0; i < choiceButtons.length; i++) {
+        choiceButtons[i].textContent = questions[currentQuestion].choices[i]
+            
+        }
+    }
